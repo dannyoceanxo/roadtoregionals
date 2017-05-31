267497f162918307df8488abf61074cf1dde6ffd
Daniel Shenker <danshenk619@gmail.com>
2017-05-31 16:05:37 -0400

    more colors

--- src/components/results/Output172.js
+++ src/components/results/Output172.js
@@ -15,11 +15,13 @@ class Output172 extends Component {
             <h2>Your score: 150 reps</h2>
             <h2>Minimum score needed to make it to Regionals: 190 reps</h2>
           </div>
-          <h3>17.2 was largely based on your ability to pace yourself as well as do gymnastic movements while fatigued.  Here's a test workout for a similar timeframe:</h3>
-          <h4>Cindy: 20 min AMRAP</h4>
-          <h4>5 pull-ups</h4>
-          <h4>10 push-ups</h4>
-          <h4>15 squats</h4>
+          <div className='training172'>
+            <h3>17.2 was largely based on your ability to pace yourself as well as do gymnastic movements while fatigued.  Here's a test workout for a similar timeframe:</h3>
+            <h4>Cindy: 20 min AMRAP</h4>
+            <h4>5 pull-ups</h4>
+            <h4>10 push-ups</h4>
+            <h4>15 squats</h4>
+          </div>
           <div>{store.resultFor('172').result.split('\n').map((p, i) => <p key={i}>{p}</p>)}</div>
         </div>
       </div>
